292c53c2cda646669f02c36315ba30b689f024b3
Daniel Sánchez Vargas <danielpat433@gmail.com>
2025-06-01 09:55:10 -0600

    Update Protocol.java

--- src/main/java/com/imaginationHoldings/protocol/Protocol.java
+++ src/main/java/com/imaginationHoldings/protocol/Protocol.java
@@ -12,4 +12,11 @@ public class Protocol {
     public static final String EDIT_ROOM = "EDIT_ROOM";
     public static final String RESERVE_ROOM = "RESERVE_ROOM";
     public static final String GET_ALL_ROOMS_ID= "GET_ALL_ROOMS_ID";
+    public static final String GET_BOOKINGS = "GET_BOOKINGS";
+    public static final String EDIT_BOOKING = "EDIT_BOOKING";
+    public static final String CANCEL_RESERVATION = "CANCEL_RESERVATION";
+    public static final String EDIT_RESERVATION = "EDIT_RESERVATION";
+    public static final String EDIT_GUEST= "EDIT_GUEST";
+    public static final String DELETE_GUEST = "DELETE_GUEST";
+
 }
